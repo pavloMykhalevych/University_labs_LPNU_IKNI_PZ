48921e2b4f4603f3b957f5739dbc67dc33aa88e2
Pavlo Mykhalevych <pmykhalevych07@gmail.com>
2021-10-07 13:27:37 +0300

    Add labs for ASD and AK.Add pract for OS.

---
+++ ak/labs/lab6/lab_6_asembler/AK_lab_6_asembler/lab_6_source.asm
@@ -0,0 +1,34 @@
+.586P
+
+.MODEL FLAT, STDCALL
+
+_DATA SEGMENT
+Num1 DD 17, 3, -51, 242, -113, -43, 78, 50, 100, -100, 4, 8, 12, 32, 34, 45, 65 
+N DD 17 
+Sum DD 0 
+Count DD 0 
+_DATA ENDS
+
+_TEXT SEGMENT 
+START:
+	lea EBX, Num1 
+	mov EDX, 0
+	mov ECX, N 
+	mov EAX, 0 
+	M1: 
+		mov EAX, [EBX]
+		cmp EAX, -43 
+		jl L2
+		cmp EAX, 78
+		jg L2
+		add SUM, EAX
+		add EBX, 4 
+		inc EDX
+	L2:
+		loop M1 
+
+		mov EAX, EDX
+		RET 
+_TEXT ENDS 
+END START
+
---
+++ os/pract/pract_1/OS_pract/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+#include <windows.h>
+#include <mutex>
+int sum = 1000;
+std::mutex mu;
+
+void university() {
+	for (auto i = 0; i < 100000; i++) {
+		mu.lock();
+		sum++;
+		mu.unlock();
+	}
+	
+}
+
+void shop() {
+	for (auto i = 0; i < 80000; i++) {
+		mu.lock();
+		sum--;
+		mu.unlock();
+	}
+	
+}
+
+int main() {
+	//int sum = 1000;
+	/*std::thread t1(university);
+	std::thread t2(shop);
+	t1.join();
+	t2.join();*/
+	HANDLE myhandle{};
+	HANDLE myhandle2{};
+	DWORD mythreadid{};
+	myhandle = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&university, 0, 0, &mythreadid);
+	myhandle2 = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&shop, 0, 0, &mythreadid);
+	WaitForSingleObject(myhandle, INFINITE);
+	WaitForSingleObject(myhandle2, INFINITE);
+	CloseHandle(myhandle);
+	CloseHandle(myhandle2);
+	std::cout << sum;
+	
+	return 0;
+}
