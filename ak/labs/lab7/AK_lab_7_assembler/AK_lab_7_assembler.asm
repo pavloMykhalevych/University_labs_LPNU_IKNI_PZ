6b523d02848ab33d980f96a7b17656cdfac9633d
Pavlo Mykhalevych <pmykhalevych07@gmail.com>
2021-10-16 16:07:08 +0300

    Finished AK lab 7

--- ak/labs/lab7/AK_lab_7_assembler/AK_lab_7_assembler.asm
+++ ak/labs/lab7/AK_lab_7_assembler/AK_lab_7_assembler.asm
@@ -5,15 +5,15 @@
 
 _DATA SEGMENT
 
-Arr1 dd 17, 3, -51, 242, -113, -43
-  dd 78, 50, 100, -100, 4, 8
-  dd 15, 32, 34, 45, 65, 4
+Arr1 dd 17, 3, -51, 2, 3, -43
+  dd 78, 50, 100, 10, 4, 8
+  dd 15, 32, 34, 1, 5, 4
   dd 5, 6, 7, 8, 9, 10
-  dd 11, 12, 13, 14, 16, -1
-  dd -1, -32, -42, 114, 101, 20
-  dd 21, 22, 23, 24, 25, 26
-  dd 27, 28, 29, 30, 31, 32
-  dd 33, 35, 36, 37, 38, 39
+  dd 11, 12, 13, 4, 3, -1
+  dd -1, -32, -42, 1, 1, 20
+  dd 21, 22, 23, 1, 7, 26
+  dd 27, 28, 29, 1, 2, 32
+  dd 33, 35, 36, 8, 1, 39
 Arr2 dd 56 dup(0)
 Arr3 dd 56 dup(0)
 
@@ -22,7 +22,8 @@ ColCount db 6
 
 Row dd 9+1
 
-SumOf9Column dd 0
+SumOf9Row dd 0
+SkalarMultof5and4Column dd 0
 
 i dd 0
 
@@ -41,7 +42,6 @@ START:
 
  lea ebx, Arr1
  lea edx, Arr2
- mov eax, 10
 
  ;copying values from arr1 to arr2
  copy:
@@ -87,8 +87,67 @@ START:
   add ebx, 4
 
  loop sum
- ;===================================================================
 
+ mov SumOf9Row, eax
+ ;===================================================================
+ ;calculating skalar multiplying of 5 and 4 columns
+
+ lea ebx, Arr1
+ add ebx, 4*3
+
+ mov ecx, 0
+ mov cl, RowCount
+ mov eax, 0
+
+ mult:
+	mov eax, [ebx]
+	add ebx, 4
+	mov edx, [ebx]
+	mul edx
+
+	add SkalarMultof5and4Column, eax
+
+	add ebx, 4*5
+
+ loop mult
+ ;===================================================================
+ ;Change 7 and 8 rows
+
+
+ mov ecx, 0
+ mov cl, ColCount
+
+ lea ebx, Arr1
+ lea edx, Arr2
+ add ebx, 4*6*6
+ add edx, 4*6*7
+
+ ;copying values from 7 to 8
+ copy78:
+  mov eax, [ebx]
+  mov [edx], eax
+
+  add ebx, 4
+  add edx, 4
+ loop copy78
+
+ mov ecx, 0
+ mov cl, ColCount
+
+ lea ebx, Arr1
+ lea edx, Arr2
+ add ebx, 4*6*7
+ add edx, 4*6*6
+
+ ;copying values from 8 to 7
+ copy87:
+  mov eax, [ebx]
+  mov [edx], eax
+
+  add ebx, 4
+  add edx, 4
+ loop copy87
+ ;===================================================================
 RET
 _TEXT ENDS
 END START
